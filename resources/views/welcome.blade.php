c9ad98282c19aee2e660d16c7d061f77e303b31a
Yewdev <Y_cooldev@outlook.com>
2022-04-02 05:33:12 +0700

    Update Header, Hero Design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,90 +13,211 @@
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"> -->
     <!-- Swiper -->
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+    <!-- Bootstrap 4 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Font Awesome 5 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
 </head>
 
-<body class="welcome-body">
+<body>
+
+    <div class="fade-in-welcome" style="position: absolute;z-index:9999;top:0;background-color:white; height: 100vh;width: 100vw;"></div>
+
+
     <!-- Header -->
     <div class="welcome-header">
-        <div class="logo">
-            <div class="img">
-                <img src="https://scontent.fnak4-1.fna.fbcdn.net/v/t39.30808-6/272925602_240603594904077_4660761821876637428_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=I0jGwycXviAAX_Wq9-Y&_nc_oc=AQm7uWlhyzOli7bnqauHV47RbQgXX8A48KOXmuzGM96errfGwq26FvzOyr0YiL9lKec037Gxh6ypcXrqIHTAVs0c&_nc_ht=scontent.fnak4-1.fna&oh=00_AT8WfSbZlPfSLlyBmmsFtRj9ljyBlQS3dVz7u-o2OUbbxg&oe=623B5A65" alt="">
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex align-items-center p-2">
+                <img class="logo" src="https://scontent.fnak4-1.fna.fbcdn.net/v/t39.30808-6/272925602_240603594904077_4660761821876637428_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=18tEdXjPqL4AX_eDmdJ&_nc_oc=AQnoCcJD-zE35kSOcuhbJfVRdM29QuZIg4p9CQV2BzWHdLC9VunZGLgO1TlXZEEkSpXXjlObODUc4VUf-0Frsj7Q&_nc_ht=scontent.fnak4-1.fna&oh=00_AT-8hdAku-m_YUfEeOo3T4lCFZ6sbEbBSyyoK4ExM_V8aA&oe=62493225" alt="">
+                <div class="pl-2">
+                    การประชุมทางวิชาการเครือข่ายอุทยานธรณีประเทศไทย ครั้งที่ 1
+                </div>
             </div>
-            <div class="title">
-                การประชุมทางวิชาการเครือข่ายอุทยานธรณีประเทศไทย ครั้งที่ 1
-            </div>
-        </div>
-        <div class="banner">
-            <div class="img">
-                <img src="https://www.geoparc.cat/wp-content/uploads/2021/07/geopark_blue_eng-400x302.png" alt="">
-            </div>
-            <div class="img">
-                <img src="https://upload.wikimedia.org/wikipedia/en/a/a6/Global_Geoparks_Network_logo.jpg" alt="">
-            </div>
-            <div class="img">
-                <img src="https://pbs.twimg.com/profile_images/1029248583332069376/89O4pUxX_400x400.jpg" alt="">
+            <!-- Mobile Navigation Icon -->
+            <div class="mobile-icon p-3">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
             </div>
         </div>
-    </div>
-    <!-- Sub Header -->
-    <div class="welcome-sub-header">
-        The 1st Thailand Geopark Network Symposium
+        <img class="top-banners" src="https://www.jejuggn2020.org/images/pages/top-banner-all.png" alt="">
     </div>
 
-    <!-- Menu Bar -->
-    <div class="welcome-menu">
-        @for ($i = 0; $i < 7; $i++) <a href="#">
-            <div class="menu-link">Link {{$i+1}}</div>
-            </a>
-            @endfor
-    </div>
+    <!-- Line break -->
+    <div class="bg-dark w-100" style="height:1px;"></div>
+
+    <!-- Sub Header -->
+    <!-- <div class="py-2" style="font-size: 12px;">
+        December 12-16, 2021/ Jeju Island UNESCO Global Geopark, Republic of Korea
+    </div> -->
+
+
+
+
+
+    <!-- Menu bar (responsive) -->
+    <div class="menu-bar-overlay"></div>
+    <ul class="menu-bar">
+        <li><a href="#">Home</a></li>
+        <li>
+            <a href="#">Programme <i class="fa fa-caret-down"></i></a>
+            <ul>
+                <a href="#">
+                    <li>Schedule / Floor Plan</li>
+                </a>
+                <a href="#">
+                    <li>Special Programme</li>
+                </a>
+            </ul>
+        </li>
+
+        <li><a href="#">Registration</a></li>
+
+        <li>
+            <a href="#">Abstract <i class="fa fa-caret-down"></i></a>
+            <ul>
+                <a href="#">
+                    <li>Poster Session</li>
+                </a>
+                <a href="#">
+                    <li>Special Session Description</li>
+                </a>
+            </ul>
+        </li>
+
+        <li><a href="#">Geofair</a></li>
+
+        <li>
+            <a href="#">Board <i class="fa fa-caret-down"></i></a>
+            <ul>
+                <a href="#">
+                    <li>News</li>
+                </a>
+                <a href="#">
+                    <li>Photo Gallery</li>
+                </a>
+            </ul>
+        </li>
+
+        <li>
+            <a href="#">Information <i class="fa fa-caret-down"></i></a>
+            <ul>
+                <a href="#">
+                    <li>JEJU Island</li>
+                </a>
+                <a href="#">
+                    <li>General Information</li>
+                </a>
+                <a href="#">
+                    <li>Visa Information</li>
+                </a>
+                <a href="#">
+                    <li>Conference Venue</li>
+                </a>
+                <a href="#">
+                    <li>Geotrials on JEJU Island</li>
+                </a>
+                <a href="#">
+                    <li>Getting to JEJU</li>
+                </a>
+                <a href="#">
+                    <li>Partnership</li>
+                </a>
+            </ul>
+        </li>
+    </ul>
+
 
 
     <!-- Slider main container -->
-    <div class="swiper" >
+    <div class="swiper">
         <!-- Additional required wrapper -->
-        <div class="swiper-wrapper" style="height:500px;">
+        <div class="swiper-wrapper">
             <!-- Slides -->
             <div class="swiper-slide">
-                <img style="object-position: 0px -350px;width:100%;" 
-                src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80" alt="">
+                <img style="width:100%;" src="{{asset('images/bg_1.png')}}" alt="">
             </div>
-            <div class="swiper-slide">
-                <img style="object-position: 0px -160px;width:100%;" 
-                src="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2174&q=80" alt="">
-            </div>
-            
+
         </div>
-        <!-- If we need pagination -->
-        <div class="swiper-pagination"></div>
-
-        <!-- If we need navigation buttons -->
+        <!-- <div class="swiper-pagination"></div>
         <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-
+        <div class="swiper-button-next"></div> -->
+        <div class="hero-text">
+            <div class="title">
+                Thailand Geoparks Network Symposium
+            </div>
+            <div class="description">
+                December 12-16, 2021/ Jeju Island UNESCO Global Geopark, Republic of Korea
+            </div>
+            <a href="#test-content">
+                <div class="scroll-down-btn fa fa-chevron-down"></div>
+            </a>
+        </div>
     </div>
 
+    <div class="test-content" id="test-content" style="height: 900px;background-color:#333;overflow:hidden;"></div>
 
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
     <script>
+        const mobileLink = document.querySelector('.mobile-icon');
+        const menuBar = document.querySelector('.menu-bar');
+        const menuBarOverlay = document.querySelector('.menu-bar-overlay');
+
+        setTimeout(function() {
+            document.querySelector('.fade-in-welcome').style.display = "none";
+        }, 3000);
+        mobileLink.addEventListener('click', function(e) {
+            mobileLink.classList.toggle("active");
+            menuBar.classList.toggle("active");
+            menuBarOverlay.classList.toggle("active");
+
+        })
+        menuBarOverlay.addEventListener('click', function(e) {
+            mobileLink.classList.toggle("active");
+            menuBar.classList.toggle("active");
+            menuBarOverlay.classList.toggle("active");
+        })
+        // ----- Sample code if click outside will hide menu ----
+        // if (!menuBar.contains(event.target)) {
+        //     mobileLink.classList.toggle("active");
+        //     menuBar.classList.toggle("active");
+        // }
+
         const swiper = new Swiper('.swiper', {
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            autoplay: {
-                delay: 4000,
-            },
+            // loop: true,
+            // pagination: {
+            //     el: '.swiper-pagination',
+            // },
+            // navigation: {
+            //     nextEl: '.swiper-button-next',
+            //     prevEl: '.swiper-button-prev',
+            // },
+            // autoplay: {
+            //     delay: 4000,
+            // },
 
         });
     </script>
+
 </body>
 
 </html>
