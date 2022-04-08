6a93a71ebdc13c4ea64998282f851e5f8d68b22c
Yewdev <Y_cooldev@outlook.com>
2022-04-08 14:46:26 +0700

    Update Responsive Underhero section (news, schedule)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -185,24 +185,26 @@
         </div>
     </div>
 
-
-    <div class="underhero-container" id="underhero-section">
-        <!-- Latest News -->
-        <div class="news-container">
-            <div class="news-icon">
-                <i class="fa fa-bullhorn"></i>
-            </div>
-            <div class="swiper newsSwiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="news-item">[12/12/2012] 1 Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="news-item">[11/12/2012] 2 Lorem ipsum dolor sit amet. Lorem, ipsum dolor.</div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="news-item">[10/12/2012] 3 Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
-                    </div>
+    <div class="section-container">
+        <div class="underhero-container" id="underhero-section">
+            <!-- Latest News -->
+            <div class="news-container">
+                <div class="title">
+                    <span class="news-icon">
+                        <i class="fa fa-bullhorn"></i>
+                    </span>
+                    Latest News
+                </div>
+                <div class="news-mobile-icon">
+                    <i class="fa fa-bullhorn"></i>
+                </div>
+                <div class="swiper newsSwiper">
+                    <div class="swiper-wrapper">
+                        @for($i=0; $i<3; $i++) 
+                        <div class="swiper-slide">
+                            <div class="news-item">[12/12/2012] 1 Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</div>
+                        </div>
+                        @endfor
                 </div>
             </div>
         </div>
@@ -210,22 +212,22 @@
         <!-- Schedule -->
 
         <div class="schedule-container">
+            <div class="title">
+                <span class="schedule-icon">
+                    <i class="fa fa-calendar"></i>
+                </span>
+                Schedule
+            </div>
             <table class="schedule-table">
+                @for($i=0; $i<3; $i++) 
                 <tr>
                     <td>Conference Registration</td>
                     <td>24 ส.ค. 65</td>
                 </tr>
-                <tr>
-                    <td>Conference Registration</td>
-                    <td>25 ส.ค. 65</td>
-                </tr>
-                <tr>
-                    <td>Conference Registration</td>
-                    <td>24 ส.ค. 65</td>
-                </tr>
+                @endfor
             </table>
         </div>
-
+    </div>
     </div>
 
     <div class="test-content" id="test-content" style="height: 900px;background-color:#333;overflow:hidden;"></div>
@@ -247,6 +249,16 @@
         const mobileLink = document.querySelector('.mobile-icon');
         const menuBar = document.querySelector('.menu-bar');
         const menuBarOverlay = document.querySelector('.menu-bar-overlay');
+
+        function responsiveFunction(xlScreen) {
+            if (xlScreen.matches) {
+                document.querySelector('.news-container .swiper').classList.remove('newsSwiper');
+                document.querySelector('.news-container .swiper').firstElementChild.classList.remove('swiper-wrapper');
+            }
+        }
+        var xlScreen = window.matchMedia("(min-width: 1200px)");
+        responsiveFunction(xlScreen);
+        // desktopScreen.addListener(myFunction);
 
         setTimeout(function() {
             document.querySelector('.fade-in-welcome').style.display = "none";
