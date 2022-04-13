abbe98a045dd486dc8581f14f72b606230f8c71d
Yewdev <Y_cooldev@outlook.com>
2022-04-13 17:39:11 +0700

    Update footer responsive

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,33 +1,36 @@
 <!-- Footer -->
 <div class="footer-section">
-    <div class="footer-container">
-        <!-- Contact -->
-        <div class="contact-container">
-            <div class="contact-content">
-                <div class="title">
-                    Thailand Geopark Network Symposium Secretariat
-                </div>
-                <div class="contact-wrapper">
-                    <div class="address">
-                        <span class="fas fa-map-marker-alt"></span>
-                        Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-                    </div>
-                    <div class="tel">
-                        <span class="fas fa-phone-alt"></span>
-                        099-888-7777
-                    </div>
-                    
-                    <div class="fax">
-                    <span class="fas fa-fax"></span>
-                        02-222-2222
-                    </div>
-                    <div class="email">
-                        <span class="fas fa-envelope"></span>
-                        contact@geopark.com
-                    </div>
-                </div>
-            </div>
+  <!-- Contact -->
+  <div class="contact-container" style="background-image: url('{{asset('images/background_images/korat1.jpg')}}');">
+    <div class="contact-content">
+      <div class="title">
+        Thailand Geopark Network Symposium Secretariat
+      </div>
+      <div class="contact-wrapper">
+        <div class="address">
+          <span class="fas fa-map-marker-alt"></span>
+          Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
         </div>
-        <div class="copyright"></div>
+        <div>
+          <div class="tel">
+            <span class="fas fa-phone-alt"></span>
+            099-888-7777
+          </div>
+
+          <div class="fax">
+            <span class="fas fa-fax"></span>
+            02-222-2222
+          </div>
+          <div class="email">
+            <span class="fas fa-envelope"></span>
+            contact@geopark.com
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+  <div class="copyright">
+    Copyright © DIGITAL 9th International Conference on UNESCO Global Geoparks. All rights reserved.
+  </div>
+
 </div>
