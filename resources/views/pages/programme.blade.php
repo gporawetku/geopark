f695f104592caf7d509adb1b88b85d37febcc0c2
Yewdev <Y_cooldev@outlook.com>
2022-04-18 18:16:40 +0700

    Update programme, all blog css(responsive)

--- resources/views/pages/programme.blade.php
+++ resources/views/pages/programme.blade.php
@@ -1,9 +1,11 @@
 @extends('../blogContent')
 
 @section('content')
-<h3>กำหนดการ</h3>
-<hr><br>
+<h3 class="text-dark-color">กำหนดการ</h3>
+<hr class="text-base-color">
+<br>
 @for($i=1; $i <= 7; $i++)
 <img width="100%" src="{{asset('images/schedules/agenda-'.$i.'.jpg')}}" alt="">
+<hr class="text-base-color my-4 mx-5">
 @endfor
 @stop
