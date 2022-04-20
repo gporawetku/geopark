4aa3b66edb99d6153176d9a6138d801d39f21aca
Guide : porawet <72738044+gporawetku@users.noreply.github.com>
2022-04-21 00:42:10 +0700

    setting adminlte and Abstract Poster

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,4 @@ Route::resource('schedules', App\Http\Controllers\ScheduleController::class);
 Route::resource('information', App\Http\Controllers\InformationController::class);
 Route::resource('geoparks', App\Http\Controllers\GeoparkController::class);
 Route::resource('geopark_images', App\Http\Controllers\GeoparkImageController::class);
-
-/*
- * AbstractPosters Routes
- */
 Route::resource('abstract_posters', App\Http\Controllers\AbstractPosterController::class);
