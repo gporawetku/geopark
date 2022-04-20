a8fb5ad27557ddd52f0d2c4717afd8177767cb85
Guide : porawet <72738044+gporawetku@users.noreply.github.com>
2022-04-21 00:50:17 +0700

    Update Name title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Geopark</title>
+    <title>Thailand Geoparks Network Symposium Secretariat</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Styles -->
@@ -33,11 +33,11 @@
 
 
     @include('layouts.header')
-    
-    
+
+
     @yield('body')
-    
-    
+
+
     @include('layouts.footer')
 
 
@@ -79,7 +79,7 @@
             menuBar.classList.toggle("active");
             menuBarOverlay.classList.toggle("active");
         })
-        
+
     </script>
     @yield('js')
 
