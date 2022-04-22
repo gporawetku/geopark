1aa19eb28fb74f1a7bfb3f282c5c93a1f2cf19f0
Yewdev <Y_cooldev@outlook.com>
2022-04-22 18:54:37 +0700

    Update Breadcrumb and navmenu css

--- resources/views/blogContent.blade.php
+++ resources/views/blogContent.blade.php
@@ -6,6 +6,20 @@
 </div>
 <div class="blog-section">
   <div class="blog-container">
+    <div class="bc">
+      <span class="fa fa-home text-dark-color"></span>
+      หน้าหลัก
+      /
+      @if(Route::current()->getName() === 'programme')
+      โปรแกรม
+      @elseif(Route::current()->getName() === 'registration')
+      ลงทะเบียน
+      @elseif(Route::current()->getName() === 'abstract')
+      บทความวิชาการ
+      @elseif(Route::current()->getName() === 'geofair')
+      จีโอพาร์คในไทย
+      @endif
+    </div>
     @yield('content')
   </div>
 </div>
