914e1667ab1eef03097d3ef325b4b88a32b6ba91
Yewdev <Y_cooldev@outlook.com>
2022-05-03 02:15:37 +0700

    Update Geotrail Rework

--- resources/views/pages/geotrail.blade.php
+++ resources/views/pages/geotrail.blade.php
@@ -6,41 +6,44 @@
 </h3>
 <hr class="text-base-color">
 
-<!-- Tabs -->
-<div class="geotrail-tabs">
-@for($i=1; $i <= 7; $i++) 
-  <button class="tab-item" 
-  onclick="showGeotrail('geotrail{{$i}}')">
-    เส้นทางท่องเที่ยวที่ {{$i}}
-  </button>
-  @endfor
-</div>
-
-<!-- Images -->
-<div class="geotrail-items">
-
-  @for($i=1; $i <= 7; $i++) 
-  <div class="geotrail-item" id="geotrail{{$i}}" style="{{$i != 1?'display: none;':''}}" >
-    <img src="{{asset('images/geotrail/0'.$i.'.jpg')}}" alt="">
+<div class="geotrail-container">
+  <!-- Tabs -->
+  <div class="geotrail-tabs">
+    @for($i=1; $i <= 7; $i++) 
+    <button class="tab-item {{$i === 1?'active':''}}"  data-geotrail="{{$i}}">
+      เส้นทางที่ {{$i}}
+    </button>
+    @endfor
   </div>
-  @endfor
-  
+
+  <!-- Images -->
+  <div class="geotrail-items">
+
+    @for($i=1; $i <= 7; $i++) 
+    <div class="geotrail-item {{$i === 1?'active':''}}" id="geotrail{{$i}}"  >
+      <img src="{{asset('images/geotrail/0'.$i.'.jpg')}}" alt="">
+    </div>
+    @endfor
+    
+  </div>
 </div>
+
 
 @stop
 
 @section('js')
 <script>
-  
-  function showGeotrail(geoNo) {
-    let geoItem = document.getElementsByClassName("geotrail-item");
-    for (let i = 0; i < geoItem.length; i++) {
-      geoItem[i].style.display = "none";
-      // geoItem[i].style.opacity = "0";
-    }
-    // document.getElementById(geoNo).style.opacity = "1";
-    document.getElementById(geoNo).style.display = "block";
+  document.querySelectorAll('.tab-item').forEach(item => {
+    item.addEventListener('click', e => {
+      // change active button
+      document.querySelectorAll('.tab-item').forEach(x => x.classList.remove('active'));
+      e.target.classList.add('active');
+      // change active img
+      document.querySelectorAll('.geotrail-item').forEach(x => x.classList.remove('active'));
+      document.querySelector(`#geotrail${e.target.dataset.geotrail}`).classList.add('active');
+      console.log(e.target.dataset.geotrail);
+    });
+  });
 
-  }
 </script>
 @stop
